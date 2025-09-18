a22c398c76b51dd37cda6a72a7fc22a8de4ae4c1
MarkNemeth05 <music.m.spoty@gmail.com>
2025-09-18 09:08:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const state = {
   unlockedMax: 1
 };
 
-const SPAWN_MS = 3000;
+const SPAWN_MS = 5 * 60 * 1000;
 let W = 540, H = 700;        // logical canvas size (updated on resize)
 
 // persistence
